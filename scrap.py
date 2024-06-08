f21a187c9d054decd037b7bb72165fd365a11af7
Ridish008 <152248673+Ridish008@users.noreply.github.com>
2024-06-08 11:14:15 +0530

    Update scrap.py
    updated code

--- scrap.py
+++ scrap.py
@@ -2,27 +2,21 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-# URL to scrape
-url = 'https://webscraper.io/test-sites/e-commerce/allinone'
 
-# Fetch the data from the URL
+url = 'https://webscraper.io/test-sites/e-commerce/allinone'
 response = requests.get(url)
 
-# Check if the response status is OK
+
 if response.status_code == 200:
     print("Data fetched successfully!")
-    # Parse the HTML content
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Find all elements with the class "pull-right price"
     price_elements = soup.find_all('h4', class_='pull-right')
     
-    # Open a CSV file to write the prices
     with open('prices.csv', 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['Price'])  # Write the header row
+        csvwriter.writerow(['Price']) 
         
-        # Write each price to the CSV file
         for price in price_elements:
             csvwriter.writerow([price.text])
     
